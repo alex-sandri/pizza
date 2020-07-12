21cdab8916ced28a7be544131dce3029eb8a4264
Alex Sandri <me@alexsandri.com>
2020-07-12 13:05:41 +0200

    Add --init option

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,10 @@ import { program } from "commander";
 
 program
     .version(pkg.version)
+    .option("-i, --init", "Create ingredients.pizza config file")
     .parse(process.argv);
+
+if (program.init)
+{
+    console.log(__dirname);
+}
