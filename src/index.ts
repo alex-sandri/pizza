829830ef6b9057b663216a5dc850421bfebc11f6
Alex Sandri <me@alexsandri.com>
2020-07-12 13:53:25 +0200

    Start implementing 'make' option

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,30 @@ import * as fs from "fs";
 const pkg = require("../package.json");
 
 import { program } from "commander";
+import * as chalk from "chalk";
+
+const CONFIG_FILE_NAME = "ingredients.pizza";
+const CONFIG_FILE_PATH = path.join(__dirname, CONFIG_FILE_NAME);
 
 program
     .version(pkg.version)
-    .option("-i, --init", "Create ingredients.pizza config file")
+    .option("-i, --init", `Create ${CONFIG_FILE_NAME} config file`)
+    .option("-m, --make", `Create config files and a basic folder structure based on the '${CONFIG_FILE_NAME}' config`)
     .parse(process.argv);
 
 if (program.init)
 {
-    fs.writeFileSync(path.join(__dirname, "ingredients.pizza"), JSON.stringify({
+    fs.writeFileSync(CONFIG_FILE_PATH, JSON.stringify({
         bundler: "webpack",
         linter: "eslint",
         templateEngine: "handlebars"
     }, null, 4));
+}
+else if (program.make)
+{
+    if (!fs.existsSync(CONFIG_FILE_PATH))
+    {
+        console.log(chalk.red("Error:"), `Cannot find '${CONFIG_FILE_NAME}' file`);
+        console.log("You need to run 'pizza --init' first");
+    }
 }
