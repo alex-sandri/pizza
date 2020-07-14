1c14a9f3a8d4a779e9bfc9530211ad43a9438699
Alex Sandri <me@alexsandri.com>
2020-07-14 10:46:04 +0200

    Fix: undefined path

--- src/config/defaults/src/scripts/build-handlebars.js
+++ src/config/defaults/src/scripts/build-handlebars.js
@@ -23,7 +23,7 @@ const getDirectories = (path) =>
         .filter(entry => entry.isDirectory())
         .map(entry => entry.name);
 
-const components = fs.readJSONSync().components;
+const components = fs.readJSONSync(path.join(__dirname, "..", "components", "components.json")).components;
 
 components.forEach(component =>
 {
