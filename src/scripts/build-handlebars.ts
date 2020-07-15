7e0b64282658d5556b1a4c45a62920f60d2a9c6c
Alex Sandri <me@alexsandri.com>
2020-07-15 17:10:43 +0200

    Move defaults/src/scripts to src/scripts (this stuff should be handled by the latest version of pizza not the one that created these files)

---
+++ src/scripts/build-handlebars.ts
@@ -0,0 +1,61 @@
+import * as path from "path";
+import * as fs from "fs-extra";
+import * as handlebars from "handlebars";
+import * as glob from "glob";
+
+const PROJECT_PATH = process.cwd();
+
+const PUBLIC_PATH = path.join(PROJECT_PATH, "public");
+const ASSETS_PATH = path.join(PUBLIC_PATH, "assets");
+
+/**
+ * The first file is the newest
+ */
+const sortByFileCreationTime = (a: string, b: string) =>
+{
+    const aStats = fs.statSync(a);
+    const bStats = fs.statSync(b);
+
+    return bStats.ctime.getTime() - aStats.ctime.getTime();
+}
+
+const getDirectories = (path: string) =>
+{
+    if (!fs.existsSync(path)) return;
+
+    return fs
+        .readdirSync(path, { withFileTypes: true })
+        .filter(entry => entry.isDirectory())
+        .map(entry => entry.name);
+};
+
+const components = getDirectories(path.join(PROJECT_PATH, "src", "components"));
+
+components?.forEach(component =>
+{
+    handlebars.registerPartial(
+        component,
+        handlebars.compile(
+            fs.readFileSync(path.join(PROJECT_PATH, "src", "components", component, `${component}.hbs`)).toString("utf-8"),
+        ),
+    );
+});
+
+const routes = getDirectories(path.join(PROJECT_PATH, "src", "routes"));
+
+routes?.forEach(route =>
+{
+    const data = {
+        assets: {
+            js: path.basename(glob.sync(path.join(ASSETS_PATH, "js", `${route}.*.js`)).sort(sortByFileCreationTime)[0]),
+            css: path.basename(glob.sync(path.join(ASSETS_PATH, "css", `${route}.*.css`)).sort(sortByFileCreationTime)[0]),
+        },
+    };
+
+    fs.writeFileSync(
+        path.join(PUBLIC_PATH, `${route}.html`),
+        handlebars.compile(
+            fs.readFileSync(path.join(PROJECT_PATH, "src", "routes", route, `${route}.hbs`), "utf8")
+        )(data),
+    );
+});
