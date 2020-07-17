9a336aff2b394e8527f8b33eca5aacdc83477719
Alex Sandri <me@alexsandri.com>
2020-07-17 20:05:05 +0200

    Allow customizing the typescript config

--- src/commands/config/apply.ts
+++ src/commands/config/apply.ts
@@ -1,3 +1,8 @@
+import * as fs from "fs-extra";
+import path from "path";
+
+import * as _ from "lodash";
+
 import {
     checkNodeVersion,
     getConfigOptions,
@@ -8,7 +13,17 @@ export const configApply = (cwd?: string) =>
 {
     checkNodeVersion();
 
-    getConfigOptions(cwd);
+    const configOptions = getConfigOptions(cwd);
 
     runCommand("npx tsc webpack.config.ts --esModuleInterop", cwd);
+
+    if (configOptions.override?.typescript)
+    {
+        const tsConfigPath = path.join(cwd ?? process.cwd(), "tsconfig.json");
+        const tsConfig = fs.readJSONSync(tsConfigPath);
+
+        _.merge(tsConfig, configOptions.override.typescript);
+
+        fs.writeJSONSync(tsConfigPath, tsConfig);
+    }
 }
