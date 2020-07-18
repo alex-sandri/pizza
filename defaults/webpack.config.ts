51f951ea8f7c399a747653a2fe4b678233fbf5fd
Alex Sandri <me@alexsandri.com>
2020-07-18 12:28:58 +0200

    Fix: NODE_ENV var not set

--- defaults/webpack.config.ts
+++ defaults/webpack.config.ts
@@ -3,85 +3,86 @@ import glob from "glob";
 import webpack from "webpack";
 import "webpack-dev-server";
 
-const isProduction = process.env.NODE_ENV === "production";
+export default (env, argv) =>
+{
+    const isProduction = argv.mode === "production";
 
-const config = <webpack.Configuration[]>[
-    {
-        entry: glob.sync("./src/@(components|routes)/**/*.ts").reduce((entries, entry) =>
+    return <webpack.Configuration[]>[
         {
-            const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
-
-            entries[entryName] = entry;
-
-            return entries;
-        }, {}),
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: "ts-loader",
-                },
-            ],
+            entry: glob.sync("./src/@(components|routes)/**/*.ts").reduce((entries, entry) =>
+            {
+                const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
+    
+                entries[entryName] = entry;
+    
+                return entries;
+            }, {}),
+            module: {
+                rules: [
+                    {
+                        test: /\.ts$/,
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            devtool: isProduction ? null : "inline-source-map",
+            resolve: {
+                extensions: [ ".ts", ".js" ],
+            },
+            devServer: isProduction
+                ? null
+                :
+                    {
+                        contentBase: "./public",
+                    },
+            output: {
+                filename: "[name].[contenthash].js",
+                path: path.resolve(__dirname, `${isProduction ? "dist" : "public"}/assets/js`),
+            },
         },
-        devtool: isProduction ? null : "inline-source-map",
-        resolve: {
-            extensions: [ ".ts", ".js" ],
-        },
-        devServer: isProduction
-            ? null
-            :
-                {
-                    contentBase: "./public",
-                },
-        output: {
-            filename: "[name].[contenthash].js",
-            path: path.resolve(__dirname, `${isProduction ? "dist" : "public"}/assets/js`),
-        },
-    },
-    {
-        entry: glob.sync("./src/@(components|routes)/**/*.scss").reduce((entries, entry) =>
         {
-            const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
-
-            entries[entryName] = entry;
-
-            return entries;
-        }, {}),
-        output: {
-            filename: "[name].style.js",
-            path: path.resolve(__dirname, `${isProduction ? "dist" : "public"}/assets/css`),
+            entry: glob.sync("./src/@(components|routes)/**/*.scss").reduce((entries, entry) =>
+            {
+                const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
+    
+                entries[entryName] = entry;
+    
+                return entries;
+            }, {}),
+            output: {
+                filename: "[name].style.js",
+                path: path.resolve(__dirname, `${isProduction ? "dist" : "public"}/assets/css`),
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.scss$/,
+                        use: [
+                            {
+                                loader: "file-loader",
+                                options: {
+                                    name: "[name].[contenthash].css",
+                                },
+                            },
+                            {
+                                loader: "extract-loader",
+                            },
+                            {
+                                loader: "css-loader",
+                                options: {
+                                    sourceMap: !isProduction,
+                                },
+                            },
+                            {
+                                loader: "sass-loader",
+                                options: {
+                                    sourceMap: !isProduction,
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
         },
-        module: {
-            rules: [
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: "file-loader",
-                            options: {
-                                name: "[name].[contenthash].css",
-                            },
-                        },
-                        {
-                            loader: "extract-loader",
-                        },
-                        {
-                            loader: "css-loader",
-                            options: {
-                                sourceMap: !isProduction,
-                            },
-                        },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: !isProduction,
-                            },
-                        },
-                    ],
-                },
-            ],
-        },
-    },
-];
-
-export default config;
+    ];
+};
