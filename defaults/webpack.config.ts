03f9f63494b15dbfbc385eb2664bda1129a3e61a
Alex Sandri <me@alexsandri.com>
2020-07-18 10:28:36 +0200

    Add production config for webpack

--- defaults/webpack.config.ts
+++ defaults/webpack.config.ts
@@ -72,6 +72,71 @@ const config: webpack.Configuration[] = [
             ],
         },
     },
+    {
+        mode: "production",
+        entry: glob.sync("./src/@(components|routes)/**/*.ts").reduce((entries, entry) =>
+        {
+            const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
+
+            entries[entryName] = entry;
+
+            return entries;
+        }, {}),
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: "ts-loader",
+                },
+            ],
+        },
+        resolve: {
+            extensions: [ ".ts", ".js" ],
+        },
+        output: {
+            filename: "[name].[contenthash].js",
+            path: path.resolve(__dirname, "dist/assets/js"),
+        },
+    },
+    {
+        mode: "production",
+        entry: glob.sync("./src/@(components|routes)/**/*.scss").reduce((entries, entry) =>
+        {
+            const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
+
+            entries[entryName] = entry;
+
+            return entries;
+        }, {}),
+        output: {
+            filename: "[name].style.js",
+            path: path.resolve(__dirname, "dist/assets/css"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                name: "[name].[contenthash].css",
+                            },
+                        },
+                        {
+                            loader: "extract-loader",
+                        },
+                        {
+                            loader: "css-loader",
+                        },
+                        {
+                            loader: "sass-loader",
+                        },
+                    ],
+                },
+            ],
+        },
+    },
 ];
 
 export default config;
