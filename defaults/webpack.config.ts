51f5bd5394a0e314e8af27b07e440503beea83fe
Alex Sandri <me@alexsandri.com>
2020-07-18 12:19:32 +0200

    Fix: webpack builds for dev and prod regardless of --mode option

--- defaults/webpack.config.ts
+++ defaults/webpack.config.ts
@@ -3,9 +3,10 @@ import glob from "glob";
 import webpack from "webpack";
 import "webpack-dev-server";
 
-const config: webpack.Configuration[] = [
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = <webpack.Configuration[]>[
     {
-        mode: "development",
         entry: glob.sync("./src/@(components|routes)/**/*.ts").reduce((entries, entry) =>
         {
             const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
@@ -22,20 +23,22 @@ const config: webpack.Configuration[] = [
                 },
             ],
         },
-        devtool: "inline-source-map",
+        devtool: isProduction ? null : "inline-source-map",
         resolve: {
             extensions: [ ".ts", ".js" ],
         },
-        devServer: {
-            contentBase: "./public",
-        },
+        devServer: isProduction
+            ? null
+            :
+                {
+                    contentBase: "./public",
+                },
         output: {
             filename: "[name].[contenthash].js",
-            path: path.resolve(__dirname, "public/assets/js"),
+            path: path.resolve(__dirname, `${isProduction ? "dist" : "public"}/assets/js`),
         },
     },
     {
-        mode: "development",
         entry: glob.sync("./src/@(components|routes)/**/*.scss").reduce((entries, entry) =>
         {
             const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
@@ -46,7 +49,7 @@ const config: webpack.Configuration[] = [
         }, {}),
         output: {
             filename: "[name].style.js",
-            path: path.resolve(__dirname, "public/assets/css"),
+            path: path.resolve(__dirname, `${isProduction ? "dist" : "public"}/assets/css`),
         },
         module: {
             rules: [
@@ -65,79 +68,14 @@ const config: webpack.Configuration[] = [
                         {
                             loader: "css-loader",
                             options: {
-                                sourceMap: true,
+                                sourceMap: !isProduction,
                             },
                         },
                         {
                             loader: "sass-loader",
                             options: {
-                                sourceMap: true,
+                                sourceMap: !isProduction,
                             },
-                        },
-                    ],
-                },
-            ],
-        },
-    },
-    {
-        mode: "production",
-        entry: glob.sync("./src/@(components|routes)/**/*.ts").reduce((entries, entry) =>
-        {
-            const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
-
-            entries[entryName] = entry;
-
-            return entries;
-        }, {}),
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: "ts-loader",
-                },
-            ],
-        },
-        resolve: {
-            extensions: [ ".ts", ".js" ],
-        },
-        output: {
-            filename: "[name].[contenthash].js",
-            path: path.resolve(__dirname, "dist/assets/js"),
-        },
-    },
-    {
-        mode: "production",
-        entry: glob.sync("./src/@(components|routes)/**/*.scss").reduce((entries, entry) =>
-        {
-            const entryName = `${path.basename(entry).split(".")[0]}.${path.basename(entry).split(".")[1]}`;
-
-            entries[entryName] = entry;
-
-            return entries;
-        }, {}),
-        output: {
-            filename: "[name].style.js",
-            path: path.resolve(__dirname, "dist/assets/css"),
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: "file-loader",
-                            options: {
-                                name: "[name].[contenthash].css",
-                            },
-                        },
-                        {
-                            loader: "extract-loader",
-                        },
-                        {
-                            loader: "css-loader",
-                        },
-                        {
-                            loader: "sass-loader",
                         },
                     ],
                 },
