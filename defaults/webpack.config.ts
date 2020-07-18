937fe4ceb0dd803143db104a14426ce9994537b0
Alex Sandri <me@alexsandri.com>
2020-07-18 10:55:30 +0200

    Emit source maps in dev builds

--- defaults/webpack.config.ts
+++ defaults/webpack.config.ts
@@ -22,6 +22,7 @@ const config: webpack.Configuration[] = [
                 },
             ],
         },
+        devtool: "inline-source-map",
         resolve: {
             extensions: [ ".ts", ".js" ],
         },
@@ -63,9 +64,15 @@ const config: webpack.Configuration[] = [
                         },
                         {
                             loader: "css-loader",
+                            options: {
+                                sourceMap: true,
+                            },
                         },
                         {
                             loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                            },
                         },
                     ],
                 },
